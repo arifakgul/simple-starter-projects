1336ecba2d3783d70bff6235467a1be82bbe49f8
Arif Emin Akgül <aro340434@gmail.com>
2024-09-25 18:00:51 +0300

    Update İndeks Hesaplama.py

--- İndeks Hesaplama.py
+++ İndeks Hesaplama.py
@@ -4,13 +4,13 @@ b=float(input("Boyunuz(m):"))
 indeks= a / (b * b)
 
 if (indeks<18.5):
-    print("İndeksiniz: {}".format(indeks),"-------> Zargana burayı terket☺")
+    print("İndeksiniz: {}".format(indeks),"-------> Çok Zayıfsınız.")
 
 elif (18.5<indeks<25):
-    print("İndeksiniz: {}".format(indeks),"-------> Normalsin aferin ula")
+    print("İndeksiniz: {}".format(indeks),"-------> İdeal kilonuzdasınız.")
 
 elif (25<indeks<30):
-    print("İndeksiniz: {}".format(indeks),"------->  Az ye az lan")
+    print("İndeksiniz: {}".format(indeks),"------->  Fazla kilolusunuz.")
 
 elif (indeks>30):
-    print("İndeksiniz: {}".format(indeks),"-------> Öküz!")
+    print("İndeksiniz: {}".format(indeks),"-------> Uyarı!! Bir diyet doktoru ile görüşün.")
